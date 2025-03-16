0b41b1d3aec52c67df882915a8da9ae77045f0bd
Janvi Gogiya <jahnvigogia@gmail.com>
2025-03-16 22:04:20 +0530

    13th March

--- src/Daily_Tasks/March_13th.java
+++
@@ -1,7 +0,0 @@
-package Daily_Tasks;
-
-public class March_13th {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Daily_Tasks/Task_13th_March.java
@@ -0,0 +1,18 @@
+package Daily_Tasks;
+import java.util.Scanner;
+
+public class Task_13th_March {
+    public static void main(String[] args) {
+        //Grade Calculator
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Score: ");
+        int Score = scanner.nextInt();
+
+        String Grade = (Score >= 90) ? "A" :
+                (Score >= 80) ? "B" :
+                        (Score >= 70) ? "C" :
+                                (Score >= 60) ? "D" : "F";
+
+        System.out.println("Your Grade is\t"+Grade);
+    }
+}
