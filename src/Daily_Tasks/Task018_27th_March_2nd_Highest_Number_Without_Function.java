ff600da3d95ba29fcfcc990e4093c0a80eeccfb3
Janvi Gogiya <jahnvigogia@gmail.com>
2025-04-01 17:22:44 +0530

    27th March

---
+++ src/Daily_Tasks/Task018_27th_March_2nd_Highest_Number_Without_Function.java
@@ -0,0 +1,41 @@
+package Daily_Tasks;
+
+public class Task018_27th_March_2nd_Highest_Number_Without_Function {
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 34, 10, 1, 100, 3, 4, 32};
+
+        int Largest = 0;
+        int Second_Largest = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > Largest) {
+                Second_Largest = Largest;
+                Largest = numbers[i];
+            } else if (numbers[i] > Second_Largest && numbers[i] != Largest ) {
+                Second_Largest = numbers[i];
+            }
+        }
+        System.out.println("First Largest Number is: "+Largest);
+        System.out.println("Second Largest Number is: "+Second_Largest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
