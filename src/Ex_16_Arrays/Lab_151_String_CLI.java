8674e83e817e59672e35065f0f1beecb7bbfdfab
Janvi Gogiya <jahnvigogia@gmail.com>
2025-04-02 12:22:46 +0530

    27th March

---
+++ src/Ex_16_Arrays/Lab_151_String_CLI.java
@@ -0,0 +1,9 @@
+package Ex_16_Arrays;
+
+public class Lab_151_String_CLI {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length ; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
